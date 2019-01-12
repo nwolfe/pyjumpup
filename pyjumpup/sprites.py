edd4221d90d61487b45893e993a5d2979912fe30
Nate Wolfe <a1979shakedown@gmail.com>
2019-01-11 23:01:08 -0800

    Character animation

--- pyjumpup/sprites.py
+++ pyjumpup/sprites.py
@@ -14,6 +14,7 @@ class Spritesheet:
         image = pg.Surface((width, height))
         image.blit(self.spritesheet, (0, 0), (x, y, width, height))
         image = pg.transform.scale(image, (width // 2, height // 2))
+        image.set_colorkey(BLACK)
         return image
 
 
@@ -21,16 +22,37 @@ class Player(pg.sprite.Sprite):
     def __init__(self, game):
         pg.sprite.Sprite.__init__(self)
         self.game = game
-        # bunny1_ready.png
-        self.image = self.game.spritesheet.get_image(614, 1063, 120, 191)
-        self.image.set_colorkey(BLACK)
-        self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.walking = False
+        self.jumping = False
+        self.current_frame = 0
+        self.last_update = 0
         self.pos = pg.math.Vector2(WIDTH / 2, HEIGHT / 2)
         self.vel = pg.math.Vector2(0, 0)
         self.acc = pg.math.Vector2(0, 0)
+        self.standing_frames = None
+        self.walk_frames_r = None
+        self.walk_frames_l = None
+        self.jump_frame = None
+        self._load_images()
+        self.image = self.standing_frames[0]
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def _load_images(self):
+        # bunny1_ready.png, bunny1_stand.png
+        self.standing_frames = [self.game.spritesheet.get_image(614, 1063, 120, 191),
+                                self.game.spritesheet.get_image(690, 406, 120, 201)]
+        # bunny1_walk1.png, bunny1_walk2.png
+        self.walk_frames_r = [self.game.spritesheet.get_image(678, 860, 120, 201),
+                              self.game.spritesheet.get_image(692, 1458, 120, 207)]
+        self.walk_frames_l = []
+        for frame in self.walk_frames_r:
+            self.walk_frames_l.append(pg.transform.flip(frame, True, False))
+        # bunny1_jump.png
+        self.jump_frame = self.game.spritesheet.get_image(382, 763, 150, 181)
 
     def update(self):
+        self.animate()
         self.acc = pg.math.Vector2(0, PLAYER_GRAVITY)
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT]:
@@ -42,13 +64,40 @@ class Player(pg.sprite.Sprite):
         self.acc.x += self.vel.x * -PLAYER_FRICTION
         # equations of motion
         self.vel += self.acc
+        if abs(self.vel.x) < 0.1:
+            self.vel.x = 0
         self.pos += self.vel + (0.5 * self.acc)
         # wrap around the sides of the screen
-        if self.pos.x > WIDTH:
-            self.pos.x = 0
-        if self.pos.x < 0:
-            self.pos.x = WIDTH
+        if self.pos.x > WIDTH + self.rect.width / 2:
+            self.pos.x = 0 - self.rect.width / 2
+        if self.pos.x < 0 - self.rect.width / 2:
+            self.pos.x = WIDTH + self.rect.width / 2
         self.rect.midbottom = self.pos
+
+    def animate(self):
+        self.walking = self.vel.x != 0
+        now = pg.time.get_ticks()
+        # show walk animation
+        if self.walking:
+            if now - self.last_update > 180:
+                self.last_update = now
+                self.current_frame = (self.current_frame + 1) % len(self.walk_frames_l)
+                bottom = self.rect.bottom
+                if self.vel.x > 0:
+                    self.image = self.walk_frames_r[self.current_frame]
+                else:
+                    self.image = self.walk_frames_l[self.current_frame]
+                self.rect = self.image.get_rect()
+                self.rect.bottom = bottom
+        # show idle animation
+        elif not self.jumping:
+            if now - self.last_update > 350:
+                self.last_update = now
+                self.current_frame = (self.current_frame + 1) % len(self.standing_frames)
+                bottom = self.rect.bottom
+                self.image = self.standing_frames[self.current_frame]
+                self.rect = self.image.get_rect()
+                self.rect.bottom = bottom
 
     def jump(self):
         # jump only if standing on a platform
