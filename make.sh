2e0794d7d8954c3cb67eb634ca2eaa6702411c1b
Nate Wolfe <a1979shakedown@gmail.com>
2019-01-18 20:57:00 -0800

    Add convenience script for building and cleaning

---
+++ make.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Usage:
+#   make.sh clean
+#   make.sh build
+
+case $1 in
+    build)
+            pyinstaller pyjumpup.spec
+            ;;
+    clean)
+            rm -rf ./build/
+            rm -rf ./dist/
+            ;;
+    *)
+            echo "Unknown: $1"
+            read
+            ;;
+esac
